ff19bb1f1843647fc1e619540cb48fd9958b8470
ingaz <221043756@mycput.ac.za>
2025-05-18 16:06:11 +0200

    Factory & Factory Test for Address, Contact and Service Review

--- src/main/java/za/ac/cput/factory/generic/AddressFactory.java
+++ src/main/java/za/ac/cput/factory/generic/AddressFactory.java
@@ -1,4 +1,34 @@
 package za.ac.cput.factory.generic;
+/* MobileGlow Car Wash
+   Factory Address
+   Author: Inga Zekani (221043756)
+ */
+
+import za.ac.cput.domain.generic.Address;
+import java.util.UUID;
+import za.ac.cput.util.Helper;
 
 public class AddressFactory {
+
+ public static Address createAddressFactory1( String streetNumber, String streetName, String city, String postalCode) {
+     //Create Unique Address ID
+     String addressID = UUID.randomUUID().toString();
+
+
+     if (!Helper.isValidString(streetNumber) ||
+             !Helper.isValidString(streetName) ||
+             !Helper.isValidString(city) ||
+             !Helper.isValidString(postalCode)) {
+         return null;
+     }
+
+     return new Address.Builder()
+             .setAddressID(addressID)
+             .setStreetNumber(streetNumber)
+             .setStreetName(streetName)
+             .setCity(city)
+             .setPostalCode(postalCode)
+             .build();
+    }
+
 }
--- src/main/java/za/ac/cput/factory/generic/ContactFactory.java
+++ src/main/java/za/ac/cput/factory/generic/ContactFactory.java
@@ -1,4 +1,27 @@
 package za.ac.cput.factory.generic;
+/* MobileGlow Car Wash
+   Factory Contact
+   Author: Inga Zekani (221043756)
+ */
+
+import za.ac.cput.domain.generic.Contact;
+import java.util.UUID;
+import za.ac.cput.util.Helper;
 
 public class ContactFactory {
+
+    public static Contact createContactFactory1(String phoneNumber) {
+        //Create Unique Contact ID
+        String contactID = UUID.randomUUID().toString();
+
+
+        if (!Helper.isValidString(phoneNumber)) {
+            return null;
+        }
+
+        return new Contact.Builder()
+                .setContactID(contactID)
+                .setPhoneNumber(phoneNumber)
+                .build();
+    }
 }
