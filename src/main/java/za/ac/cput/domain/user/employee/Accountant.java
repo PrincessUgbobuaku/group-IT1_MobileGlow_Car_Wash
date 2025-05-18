4f6703c965489d1ea416f9086e1541778e8caf3f
Abulele Voki <230778941@mycput.ac.za>
2025-05-18 14:12:48 +0200

    employee classes with the builder patterns

--- src/main/java/za/ac/cput/domain/user/employee/Accountant.java
+++ src/main/java/za/ac/cput/domain/user/employee/Accountant.java
@@ -5,29 +5,42 @@ public class Accountant {
     private String accountantID;
     private boolean hasTaxFillingAuthority;
 
-    public Accountant() {
+    public Accountant(){}
 
+    public Accountant(Builder builder){
+        this.accountantID=builder.accountantID;
+        this.hasTaxFillingAuthority=builder.hasTaxFillingAuthority;
     }
 
-    public Accountant(String accountantID, boolean hasTaxFillingAuthority) {
-        this.accountantID = accountantID;
-        this.hasTaxFillingAuthority = hasTaxFillingAuthority;
+    public String getAccountantID(){return accountantID;}
+
+    public boolean getHasTaxFillingAuthority(){return hasTaxFillingAuthority;}
+
+    @Override
+    public String toString() {
+        return "Accountant{" +
+                "accountantID='" + accountantID + '\'' +
+                ", hasTaxFillingAuthority=" + hasTaxFillingAuthority +
+                '}';
     }
 
-    public String getAccountantID() {
-        return accountantID;
-    }
+    public static class Builder{
+        private String accountantID;
+        private boolean hasTaxFillingAuthority;
 
-    public void setAccountantID(String accountantID) {
-        this.accountantID = accountantID;
-    }
+        public Builder setAccountantID(String accountantID){
+            this.accountantID=accountantID;
+            return this;
+        }
 
-    public boolean isHasTaxFillingAuthority() {
-        return hasTaxFillingAuthority;
-    }
+        public Builder setHasTaxFillingAuthority(boolean hasTaxFillingAuthority){
+            this.hasTaxFillingAuthority=hasTaxFillingAuthority;
+            return this;
+        }
 
-    public void setHasTaxFillingAuthority(boolean hasTaxFillingAuthority) {
-        this.hasTaxFillingAuthority = hasTaxFillingAuthority;
+        public Accountant build(){
+            return new Accountant(this);
+        }
     }
 
 }
--- src/main/java/za/ac/cput/domain/user/employee/WashAttendant.java
+++ src/main/java/za/ac/cput/domain/user/employee/WashAttendant.java
@@ -1,44 +1,64 @@
 package za.ac.cput.domain.user.employee;
-
+//230778941
+//Abulele Voki
 public class WashAttendant {
     private String washAttendantID;
     private boolean isFullTime;
     private int shiftHours;
 
 
-    public WashAttendant(){
-
-
-    }
-
-    public WashAttendant(String washAttendantID, boolean isFullTime, int shiftHours) {
-        this.washAttendantID = washAttendantID;
-        this.isFullTime = isFullTime;
-        this.shiftHours = shiftHours;
+    private  WashAttendant(Builder builder){
+        this.washAttendantID=builder.washAttendantID;
+        this.isFullTime = builder.isFullTime;
+        this.shiftHours=  builder.shiftHours;
     }
 
     public String getWashAttendantID() {
         return washAttendantID;
+
     }
 
-    public void setWashAttendantID(String washAttendantID) {
-        this.washAttendantID = washAttendantID;
-    }
-
-    public boolean isFullTime() {
+    public boolean getIsFullTime() {
         return isFullTime;
-    }
 
-    public void setFullTime(boolean fullTime) {
-        isFullTime = fullTime;
     }
 
     public int getShiftHours() {
         return shiftHours;
     }
 
-    public void setShiftHours(int shiftHours) {
-        this.shiftHours = shiftHours;
+    @Override
+    public String toString() {
+        return "WashAttendant{" +
+                "washAttendantID='" + washAttendantID + '\'' +
+                ", isFullTime=" + isFullTime +
+                ", shiftHours=" + shiftHours +
+                '}';
     }
 
+
+    public static class Builder {
+        private String washAttendantID;
+        private boolean isFullTime;
+        private int shiftHours;
+
+
+        public Builder setWashAttendantID(String washAttendantID) {
+            this.washAttendantID = washAttendantID;
+            return this;
+        }
+
+        public Builder setisFullTime(boolean isFullTime){
+            this.isFullTime = isFullTime;
+            return this;
+        }
+
+        public Builder setShiftHours(int shiftHours){
+            this.shiftHours = shiftHours;
+            return this;
+        }
+        public WashAttendant build(){
+            return new WashAttendant(this);
+        }
+    }
 }
