849129341189f8beb4b5bd1d6d965a4243c44d95
Abulele Voki <230778941@mycput.ac.za>
2025-05-25 21:27:41 +0200

    Employee repositorys and Repository tests

--- src/main/java/za/ac/cput/repository/user/employee/impl/IAccountantRepository.java
+++ src/main/java/za/ac/cput/repository/user/employee/impl/IAccountantRepository.java
@@ -1,4 +1,14 @@
 package za.ac.cput.repository.user.employee.impl;
 
+
+
+import za.ac.cput.domain.user.employee.Accountant;
+import java.util.List;
+
 public interface IAccountantRepository {
+    Accountant create(Accountant accountant);
+    Accountant read(String accountantID);
+    Accountant update(Accountant accountant);
+    boolean delete(String accountantID);
+    List<Accountant> getAll();
 }
