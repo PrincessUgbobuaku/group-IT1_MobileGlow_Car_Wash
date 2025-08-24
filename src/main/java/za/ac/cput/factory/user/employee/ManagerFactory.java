86ed22d03d668abeb42ab34ce200d8e15fce3f6d
Kwanda <Kwndtwalo@yahoo.com>
2025-08-24 16:52:00 +0200

    These are updated classes User, Manager, and Login. I have included the factory and their tests. Added some methods on helper class for validation.

--- src/main/java/za/ac/cput/factory/user/employee/ManagerFactory.java
+++ src/main/java/za/ac/cput/factory/user/employee/ManagerFactory.java
@@ -1,28 +1,44 @@
 package za.ac.cput.factory.user.employee;
 
+import za.ac.cput.domain.generic.Address;
+import za.ac.cput.domain.generic.Contact;
+import za.ac.cput.domain.user.Login;
+import za.ac.cput.domain.user.User;
 import za.ac.cput.domain.user.employee.Manager;
 import za.ac.cput.util.Helper;
 
-import java.util.Date;
-import java.util.UUID;
+import java.time.LocalDate;
 
 public class ManagerFactory {
-    public static Manager createManager(Date hireDate) {
-        String managerId = Helper.generateID();{
-            if(!Helper.validateDate(hireDate)) {
 
-                return null;
-            }
+    public static Manager createManager(String userName,
+                                        String userSurname,
+                                        User.RoleDescription roleDescription,
+                                        boolean isActive,
+                                        LocalDate hireDate,
+                                        String employeeType,
+                                        Contact contact,
+                                        Address address,
+                                        Login login) {
 
+        if (!Helper.validateStringDetails(userName) ||
+                !Helper.validateStringDetails(userSurname) ||
+                !Helper.isValidHireDate(hireDate) ||
+                !Helper.validateStringDetails(employeeType)) {
+            return null;
         }
 
-
-        return new Manager.Builder().
-                setHireDate(hireDate).
-                setManagerID(managerId).
-                build();
-
-
+        return new Manager.Builder()
+                .setUserName(userName)
+                .setUserSurname(userSurname)
+                .setRoleDescription(roleDescription)
+                .setIsActive(isActive)
+                .setHireDate(hireDate)
+                .setEmployeeType(employeeType)
+                .setContact(contact)
+                .setAddress(address)
+                .setLogin(login)
+                .build();
     }
 
 }
--- src/main/java/za/ac/cput/repository/user/LoginRepository.java
+++ src/main/java/za/ac/cput/repository/user/LoginRepository.java
@@ -1,10 +1,9 @@
 package za.ac.cput.repository.user;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.user.Login;
-import za.ac.cput.repository.IRepository;
 
-import java.util.Set;
-
-public interface LoginRepository extends IRepository<Login, String> {
-    Set<Login> getLogins();
+@Repository
+public interface LoginRepository extends JpaRepository<Login, Long> {
 }
