013a3eff04c7e4df80c9c9e0c8dec17faec5a979
ingaz <221043756@mycput.ac.za>
2025-08-26 00:28:01 +0200

    ServiceReviewService, AddressService and ContactService classes

--- src/main/java/za/ac/cput/service/generic/AddressService.java
+++ src/main/java/za/ac/cput/service/generic/AddressService.java
@@ -1,4 +1,52 @@
 package za.ac.cput.service.generic;
 
-public class AddressService {
-}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.generic.Address;
+import za.ac.cput.repository.generic.IAddressRepository;
+import za.ac.cput.service.generic.AddressService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AddressService implements IAddressService {
+
+    private final IAddressRepository repository;
+
+    @Autowired
+    public AddressService(IAddressRepository repository) {
+
+        this.repository = repository;
+    }
+
+
+    @Override
+    public Address create(Address address) {
+        return repository.save(address);
+    }
+
+    @Override
+    public Address read(Long aLong) {
+        return repository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Address update(Address address) {
+        return repository.save(address);
+    }
+
+    @Override
+    public boolean delete(Long aLong) {
+        repository.deleteById(aLong);
+        return true;
+    }
+
+    @Override
+    public List<Address> getAllAddresses() {
+        return repository.findAll();
+    }
+
+
+    }
+
