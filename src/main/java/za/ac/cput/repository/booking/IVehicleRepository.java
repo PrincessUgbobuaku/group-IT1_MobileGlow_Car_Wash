97652ca49ca1df32de5b7fbff332dcad2745b631
ThaakirahWatson <230037550@mycput.ac.za>
2025-08-25 14:07:21 +0200

    updated customer and vehicle repository classes

--- src/main/java/za/ac/cput/repository/booking/IVehicleRepository.java
+++ src/main/java/za/ac/cput/repository/booking/IVehicleRepository.java
@@ -1,28 +1,17 @@
+//Thaakirah Watson, 230037550
 package za.ac.cput.repository.booking;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import za.ac.cput.domain.booking.Vehicle;
-
-//Thaakirah Watson, 230037550
-
-import za.ac.cput.domain.booking.Vehicle;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.booking.Vehicle;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface IVehicleRepository extends JpaRepository<Vehicle, String> {
-
-    // already has findById
-    Optional<Vehicle> findByVehicleID(String vehicleID);
-
-    // find vehicles belonging to a customer
-    List<Vehicle> findByCustomer_CustomerID(String customerID);
-
-    // find by plate number
+public interface IVehicleRepository extends JpaRepository<Vehicle, Long> {
+    // Find vehicle by plate number
     Optional<Vehicle> findByCarPlateNumber(String carPlateNumber);
 
-
+    // Find all vehicles by customer ID
+    java.util.List<Vehicle> findByCustomer_UserId(Long userId);
 }
