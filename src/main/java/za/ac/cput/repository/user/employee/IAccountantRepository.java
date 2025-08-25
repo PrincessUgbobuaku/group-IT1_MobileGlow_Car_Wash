ca78d53805d544eb418e3a9ca6355dbe9dafa4c8
Abulele Voki <230778941@mycput.ac.za>
2025-08-25 15:44:34 +0200

    Merge remote-tracking branch 'origin/230778941' into 230778941

--- src/main/java/za/ac/cput/repository/user/employee/IAccountantRepository.java
+++ src/main/java/za/ac/cput/repository/user/employee/IAccountantRepository.java
@@ -1,4 +1,16 @@
 package za.ac.cput.repository.user.employee;
 
-public interface IAccountantRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.user.User;
+import za.ac.cput.domain.user.employee.Accountant;
+import za.ac.cput.repository.IRepository;
+
+import java.util.List;
+
+@Repository
+public interface IAccountantRepository extends JpaRepository<Accountant, Long> {
+
+
+
 }
