6fe2cdb9f65520814621b4f906d9aa767b2d83a1
ingaz <221043756@mycput.ac.za>
2025-08-26 18:58:10 +0200

    IContactService, IAddressService and IServiceReviewService

--- src/main/java/za/ac/cput/service/generic/IAddressService.java
+++ src/main/java/za/ac/cput/service/generic/IAddressService.java
@@ -1,4 +1,22 @@
 package za.ac.cput.service.generic;
 
-public interface IAddressService {
+/* MobileGlow Car Wash
+   IAddress Service
+   Author: Inga Zekani (221043756)
+ */
+
+import za.ac.cput.domain.generic.Address;
+import za.ac.cput.service.IService;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IAddressService extends IService<Address, Long> {
+
+//    Address saveAddress(Address address);
+    List<Address> getAllAddresses();
+//    Optional<Address> getAddressById(Long id);
+//    boolean existsById(Long id);
+//    void deleteAddressById(Long id);
+
 }
--- src/main/java/za/ac/cput/service/review/IServiceReviewService.java
+++ src/main/java/za/ac/cput/service/review/IServiceReviewService.java
@@ -1,4 +1,19 @@
 package za.ac.cput.service.review;
 
-public interface IServiceReviewService {
-}
+/* MobileGlow Car Wash
+   IServiceReviewService Class
+   Author: Inga Zekani (221043756)
+ */
+
+import za.ac.cput.domain.review.ServiceReview ;
+import za.ac.cput.service.IService;
+
+import java.util.List;
+
+public interface IServiceReviewService extends IService<ServiceReview, Long> {
+    List<ServiceReview> getAll();
+
+    }
+
+
+
