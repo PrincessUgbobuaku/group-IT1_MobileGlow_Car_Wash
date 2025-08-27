f14cca65e67c5a73a80687f051528d14e82e587b
ThaakirahWatson <230037550@mycput.ac.za>
2025-08-27 13:09:50 +0200

    completed issue #53, issue #59, and issue #63 for customer and vehicle classes

--- src/main/java/za/ac/cput/service/booking/IVehicleService.java
+++ src/main/java/za/ac/cput/service/booking/IVehicleService.java
@@ -1,14 +1,16 @@
+//Thaakirah Watson, 230037550
 package za.ac.cput.service.booking;
 
-import org.springframework.stereotype.Repository;
-import za.ac.cput.domain.booking.CleaningService;
+import za.ac.cput.domain.booking.Vehicle;
 import za.ac.cput.service.IService;
 
 import java.util.List;
+import java.util.Optional;
 
-@Repository
-public interface IVehicleService extends IService<CleaningService, String> {
+public interface IVehicleService extends IService<Vehicle, Long> {
+    //find vehicle by plate number
+    Optional<Vehicle> findByPlateNumber(String plateNumber);
 
-    List<CleaningService> getAll();
-
+    //find vehicle by customerId
+    List<Vehicle> findByCustomerId(Long customerId);
 }
--- src/main/java/za/ac/cput/service/user/ICustomerService.java
+++ src/main/java/za/ac/cput/service/user/ICustomerService.java
@@ -1,4 +1,15 @@
+//Thaakirah Watson, 230037550
 package za.ac.cput.service.user;
 
-public interface ICustomerService {
+import za.ac.cput.domain.user.Customer;
+import za.ac.cput.service.IService;
+
+import java.util.List;
+
+public interface ICustomerService extends IService<Customer, Long> {
+    //Find customer by surname
+    List<Customer> findBySurname(String surname);
+
+    //find active customers
+    List<Customer> findActiveCustomers();
 }
