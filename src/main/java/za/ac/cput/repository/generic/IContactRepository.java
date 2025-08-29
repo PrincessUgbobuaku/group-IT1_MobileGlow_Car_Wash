d17559c5f06f7cf7beffd1101d539e94770898c1
Abulele <165039141+vokiabulele@users.noreply.github.com>
2025-08-29 22:05:06 +0200

    Update IContactRepository.java
    Annotation Added to contactRepoistory

--- src/main/java/za/ac/cput/repository/generic/IContactRepository.java
+++ src/main/java/za/ac/cput/repository/generic/IContactRepository.java
@@ -1,4 +1,21 @@
 package za.ac.cput.repository.generic;
 
-public interface IContactRepository {
+/* MobileGlow Car Wash
+   IContact Repository
+   Author: Inga Zekani (221043756)
+ */
+
+import za.ac.cput.domain.generic.Contact;
+import za.ac.cput.repository.IRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.generic.Contact;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface IContactRepository extends JpaRepository<Contact, Long> {
+    
 }
