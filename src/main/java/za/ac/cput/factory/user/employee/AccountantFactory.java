c42279d8c7126136753cffa52b0ffec8962cc6ef
ingaz <221043756@mycput.ac.za>
2025-08-29 16:19:17 +0200

    AddressFactory, ContactFactory, and ServiceReviewFactory

--- src/main/java/za/ac/cput/factory/user/employee/AccountantFactory.java
+++ src/main/java/za/ac/cput/factory/user/employee/AccountantFactory.java
@@ -1,21 +1,45 @@
 package za.ac.cput.factory.user.employee;
 
 import za.ac.cput.domain.user.employee.Accountant;
-import za.ac.cput.util.Helper;
-
-
+import za.ac.cput.domain.generic.Address;
+import za.ac.cput.domain.generic.Contact;
+import za.ac.cput.domain.user.Login;
+import za.ac.cput.domain.user.User;
 
 public class AccountantFactory {
-    public static Accountant createAccountant(boolean hasTaxFillingAuthority) {
-        String accountantID = Helper.generateID();
-        if(!Helper.validateHasTaxFillingAuthority(hasTaxFillingAuthority)){
-            return null;
-        }
 
+    public static Accountant createAccountant(String userName, String userSurname,
+                                              boolean isActive, User.RoleDescription roleDescription,
+                                              boolean hasTaxFillingAuthority,
+                                              Contact contact, Address address, Login login) {
 
         return new Accountant.Builder()
-                .setAccountantID(accountantID)
+                .setUserName(userName)
+                .setUserSurname(userSurname)
+                .setIsActive(isActive)
+                .setRoleDescription(roleDescription)
                 .setHasTaxFillingAuthority(hasTaxFillingAuthority)
+                .setContact(contact)
+                .setAddress(address)
+                .setLogin(login)
+                .build();
+    }
+
+    public static Accountant createAccountantWithId(Long userId, String userName, String userSurname,
+                                                    boolean isActive, User.RoleDescription roleDescription,
+                                                    boolean hasTaxFillingAuthority,
+                                                    Contact contact, Address address, Login login) {
+
+        return new Accountant.Builder()
+                .setUserId(userId)
+                .setUserName(userName)
+                .setUserSurname(userSurname)
+                .setIsActive(isActive)
+                .setRoleDescription(roleDescription)
+                .setHasTaxFillingAuthority(hasTaxFillingAuthority)
+                .setContact(contact)
+                .setAddress(address)
+                .setLogin(login)
                 .build();
     }
 }
