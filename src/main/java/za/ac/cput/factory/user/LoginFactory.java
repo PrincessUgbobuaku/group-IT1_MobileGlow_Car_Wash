18cef9f92c64a1aaa77c9189ba5a094d0574ee75
Kwanda <Kwndtwalo@yahoo.com>
2025-09-24 01:13:18 +0200

    Security Implementation.

--- src/main/java/za/ac/cput/factory/user/LoginFactory.java
+++ src/main/java/za/ac/cput/factory/user/LoginFactory.java
@@ -4,20 +4,28 @@ package za.ac.cput.factory.user;
 //LastName:         Twalo
 //Student Number:   218120192.
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import za.ac.cput.domain.user.Login;
 import za.ac.cput.util.Helper;
 
 public class LoginFactory {
 
+    private static final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(12);
+
     public static Login createLogin(String emailAddress, String password) {
 
         if (!Helper.isValidEmail(emailAddress) ||
-                !Helper.isValidPassword(password)
-        )
-         {return null;}
+                !Helper.isValidPassword(password)) {
+            return null;
+        }
+
+        // Encode the password before building the Login object
+        String encodedPassword = passwordEncoder.encode(password);
+
         return new Login.Builder()
                 .setEmailAddress(emailAddress)
-                .setPassword(password)
+                .setPassword(encodedPassword)
                 .build();
     }
 
